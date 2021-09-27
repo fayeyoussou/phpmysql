c7f4d84eab7fbc3990d2a8e5ac197f09c5050c1d
Youtech <fayeyousso@gmail.com>
2021-09-27 10:58:27 +0000

    CMS-Controller

---
+++ classes/controllers/RegisterController.php
@@ -0,0 +1,13 @@
+<?php
+
+class RegisterController {
+    public function __construct ($authors)
+    {
+        
+    }
+    public function showForm()
+    {
+        
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -1,51 +1,72 @@
 <?php
-function loadTemplate($templateFileName, $variables = [])
-{
-    extract($variables);
+function loadTemplate($templateFileName, $variables = []) {
+	extract($variables);
 
-    ob_start();
-    include  __DIR__ . '/../templates/' . $templateFileName;
+	ob_start();
+	include  __DIR__ . '/../templates/' . $templateFileName;
 
-    return ob_get_clean();
+	return ob_get_clean();
 }
+
 try {
-    include __DIR__ . '/../includes/DatabaseConnection.php';
-    include __DIR__ . '/../classes/DatabaseTable.php';
-    include __DIR__ . '/../classes/controllers/JokeController.php';
+	include __DIR__ . '/../includes/DatabaseConnection.php';
+	include __DIR__ . '/../classes/DatabaseTable.php';
 
-    $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
-    $authorsTable = new DatabaseTable($pdo, 'author', 'id');
-    $jokeController = new JokeController($jokesTable, 
-    $authorsTable);
+	$jokesTable = new DatabaseTable($pdo, 'joke', 'id');
+	$authorsTable = new DatabaseTable($pdo, 'author', 'id');
 
-    $action = $_GET['action'] ?? 'home';
-    // $page = $jokeController->$action();
-    // $output = $page['output'];
-    // if (isset($page['variables'])) extract($page['variables']);
-    // ob_start();
-    // // echo ('page :');
-    // // var_dump($page);
-    // // var_dump($page['variables']);
-    // // echo $page['template'];
-    // // echo 'what';
-    // include __DIR__.'/../templates/'.$page['template'];
-    if ($action == strtolower($action)) {
-        $page = $jokeController->$action();
-    }
-    else {
-        http_response_code(301);
-        header('location: index.php?action=' .strtolower($action));
-    }
 
-    $title = $page['title'];
-    $output = loadTemplate($page['template'],$page['variables']??[]);
+	$route = $_GET['route'] ?? 'joke/home'; //if no route variable is set, use 'joke/home'
 
-    } catch (PDOException $e) {
-        $title = 'An error has occurred';
-    
-        $output = 'Database error: ' . $e->getMessage() . ' in '
-         . $e->getFile() . ':' . $e->getLine();
-    }
-    
-    include  __DIR__ . '/../templates/layout.html.php';
-    
+	if ($route == strtolower($route)) {
+
+			if ($route === 'joke/list') {
+				include __DIR__ . '/../classes/controllers/JokeController.php';
+				$controller = new JokeController($jokesTable, $authorsTable);
+				$page = $controller->list();
+			}
+			else if ($route === 'joke/home') {
+				include __DIR__ . '/../classes/controllers/JokeController.php';
+				$controller = new JokeController($jokesTable, $authorsTable);
+				$page = $controller->home();
+			}
+			else if ($route === 'joke/edit') {
+				include __DIR__ . '/../classes/controllers/JokeController.php';
+				$controller = new JokeController($jokesTable, $authorsTable);
+				$page = $controller->edit();
+			}
+			else if ($route === 'joke/delete') {
+				include __DIR__ . '/../classes/controllers/JokeController.php';
+				$controller = new JokeController($jokesTable, $authorsTable);
+				$page = $controller->delete();
+			}
+			else if ($route === 'register') {
+				include __DIR__ . '/../classes/controllers/RegisterController.php';
+				$controller = new RegisterController($authorsTable);
+				$page = $controller->showForm();
+			}
+	}
+	else {
+		http_response_code(301);
+		header('location: index.php?route=' . strtolower($route));
+	}
+
+
+	$title = $page['title'];
+
+	if (isset($page['variables'])) {
+		$output = loadTemplate($page['template'], $page['variables']);
+	}
+	else {
+		$output = loadTemplate($page['template']);
+	}
+	
+}
+catch (PDOException $e) {
+	$title = 'An error has occurred';
+
+	$output = 'Database error: ' . $e->getMessage() . ' in ' .
+	$e->getFile() . ':' . $e->getLine();
+}
+
+include  __DIR__ . '/../templates/layout.html.php';
