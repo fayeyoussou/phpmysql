765bc7b5ec5430d7ec83c7d16a58296686485275
Youtech <fayeyousso@gmail.com>
2021-10-01 11:36:30 +0000

    CMS-EntryPoint-Class

---
+++ classes/EntryPoint.php
@@ -0,0 +1,66 @@
+<?php
+
+        class EntryPoint {
+            private $route;
+            public function __construct($route) {
+                $this->route = $route;
+                $this->checkUrl();
+            }
+            private function checkUrl() {
+                if($this->route !==strtolower($this->route)){
+                    http_response_code(301);
+                    header('location : /joke/public/'.strtolower($this->route));
+                }
+            }
+            private function loadTemplate($templateFileName, $variables = []) {
+                extract($variables);
+            
+                ob_start();
+                include  __DIR__ . '/../templates/' . $templateFileName;
+            
+                return ob_get_clean();
+            }
+            private function callAction()
+            {
+                include __DIR__ . '/../includes/DatabaseConnection.php';
+	            include __DIR__ . '/../classes/DatabaseTable.php';
+                $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
+	            $authorsTable = new DatabaseTable($pdo, 'author', 'id');
+                if ($this->route === 'joke/list') {
+                    include __DIR__ . '/../classes/controllers/JokeController.php';
+                    $controller = new JokeController($jokesTable, $authorsTable);
+                    $page = $controller->list();
+                }
+                else if ($this->route === 'joke/edit') {
+                    include __DIR__ . '/../classes/controllers/JokeController.php';
+                    $controller = new JokeController($jokesTable, $authorsTable);
+                    $page = $controller->edit();
+                }
+                else if ($this->route === 'joke/delete') {
+                    include __DIR__ . '/../classes/controllers/JokeController.php';
+                    $controller = new JokeController($jokesTable, $authorsTable);
+                    $page = $controller->delete();
+                }
+                else if ($this->route === 'register') {
+                    include __DIR__ . '/../classes/controllers/RegisterController.php';
+                    $controller = new RegisterController($authorsTable);
+                    $page = $controller->showForm();
+                }else
+                {
+                    include __DIR__ . '/../classes/controllers/JokeController.php';
+                    $controller = new JokeController($jokesTable, $authorsTable);
+                    $page = $controller->home();
+                } 
+                return $page;
+            }
+            public function run() {
+                $page = $this->callAction();
+                $title = $page['title'];
+                if (isset($page['variables'])) {
+                    $output = $this->loadTemplate($page['template'],$page['variables']);
+                } else {
+                    $ouput = $this->loadTemplate($page['template']);
+                }
+                include  __DIR__ . '/../templates/layout.html.php';
+            }
+        }
--- public/index.php
+++ public/index.php
@@ -1,65 +1,11 @@
 <?php
-function loadTemplate($templateFileName, $variables = []) {
-	extract($variables);
-
-	ob_start();
-	include  __DIR__ . '/../templates/' . $templateFileName;
-
-	return ob_get_clean();
-}
 
 try {
-	include __DIR__ . '/../includes/DatabaseConnection.php';
-	include __DIR__ . '/../classes/DatabaseTable.php';
-
-	$jokesTable = new DatabaseTable($pdo, 'joke', 'id');
-	$authorsTable = new DatabaseTable($pdo, 'author', 'id');
-
-
 	$route = $_REQUEST['q'] ?? 'joke/home'; //if no route variable is set, use 'joke/home'
-
-	if ($route == strtolower($route)) {
-
-			if ($route === 'joke/list') {
-				include __DIR__ . '/../classes/controllers/JokeController.php';
-				$controller = new JokeController($jokesTable, $authorsTable);
-				$page = $controller->list();
-			}
-			else if ($route === 'joke/home') {
-				include __DIR__ . '/../classes/controllers/JokeController.php';
-				$controller = new JokeController($jokesTable, $authorsTable);
-				$page = $controller->home();
-			}
-			else if ($route === 'joke/edit') {
-				include __DIR__ . '/../classes/controllers/JokeController.php';
-				$controller = new JokeController($jokesTable, $authorsTable);
-				$page = $controller->edit();
-			}
-			else if ($route === 'joke/delete') {
-				include __DIR__ . '/../classes/controllers/JokeController.php';
-				$controller = new JokeController($jokesTable, $authorsTable);
-				$page = $controller->delete();
-			}
-			else if ($route === 'register') {
-				include __DIR__ . '/../classes/controllers/RegisterController.php';
-				$controller = new RegisterController($authorsTable);
-				$page = $controller->showForm();
-			}
-	}
-	else {
-		http_response_code(301);
-		header('location: index.php?route=' . strtolower($route));
-	}
-
-
-	$title = $page['title'];
-
-	if (isset($page['variables'])) {
-		$output = loadTemplate($page['template'], $page['variables']);
-	}
-	else {
-		$output = loadTemplate($page['template']);
-	}
+	include __DIR__ .'/../classes/EntryPoint.php';
+	$entryPoint = new EntryPoint($route);
+	// (new EntryPoint($route))->run();
+	$entryPoint->run();
 	
 }
 catch (PDOException $e) {
@@ -68,5 +14,3 @@ catch (PDOException $e) {
 	$output = 'Database error: ' . $e->getMessage() . ' in ' .
 	$e->getFile() . ':' . $e->getLine();
 }
-
-include  __DIR__ . '/../templates/layout.html.php';
