9fed7b8dec72f0f33b06e6a235965edee938864b
Youtech <fayeyousso@gmail.com>
2021-10-03 15:22:54 +0000

    Registration-Validation-Email

--- classes/Ijdb/Controllers/Register.php
+++ classes/Ijdb/Controllers/Register.php
@@ -22,8 +22,50 @@ class Register {
     }
     public function registerUser () {
         $author = $_POST['author'];
-        $this->authorsTable->save($author);
-        header('location : /joke/public/author/success');
+
+		//Assume the data is valid to begin with
+		$valid = true;
+		$errors = [];
+
+		//But if any of the fields have been left blank, set $valid to false
+		if (empty($author['name'])) {
+			$valid = false;
+			$errors[] = 'Name cannot be blank';
+		}
+
+		if (empty($author['email'])) {
+			$valid = false;
+			$errors[] = 'Email cannot be blank';
+		}
+		else if (filter_var($author['email'], FILTER_VALIDATE_EMAIL) == false) {
+			$valid = false;
+			$errors[] = 'Invalid email address';
+		}
+
+		if (empty($author['password'])) {
+			$valid = false;
+			$errors[] = 'Password cannot be blank';
+		}
+
+		//If $valid is still true, no fields were blank and the datacan be added
+		if ($valid == true) {
+			$this->authorsTable->save($author);
+
+			$this->authorsTable->save($author);
+
+			header('Location: /author/success');
+		}
+		else {
+			//If the data is not valid, show the form again
+			return ['template' => 'register.html.php', 
+				    'title' => 'Register an account',
+				    'variables' => [
+				    	'errors' => $errors,
+				    	'author' => $author
+				    ]
+				   ]; 
+		}
+	
     }
 
 }
